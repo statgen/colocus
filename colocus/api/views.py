bb1090984de5788e0778fd38b4347ce6dd3b38ab
Ryan Welch <welchr@umich.edu>
2023-03-31 15:00:11 -0400

    Allow sort on ENSG ID

--- colocus/api/views.py
+++ colocus/api/views.py
@@ -98,6 +98,7 @@ class ColocResultListView(OneStudyMixin, generics.ListAPIView):
         'signal2__lead_variant_chrom',
         'signal2__lead_variant_pos',
         'signal2__trait__metadata__gene',
+        'signal2__trait__metadata__gene_ensg',
         'signal2__trait__metadata__tissue',
         'signal1__lead_variant_nearest_gene',
         'signal2__lead_variant_assoc_gene'

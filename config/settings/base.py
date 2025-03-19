61bc4e05470c5925ae59495617288adf4c89d2e8
Ryan Welch <welchr@umich.edu>
2025-03-19 22:40:41 +0000

    Fix misconfigured filter backend

--- config/settings/base.py
+++ config/settings/base.py
@@ -261,7 +261,6 @@ LOGGING = {
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
-        'rest_framework.filters.OrderingFilter',
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     # Since the result set is stable, a basic pagination class is sufficient

42356cdc19b46de8fa8a0f50b87a439a6b886669
Ryan Welch <welchr@umich.edu>
2025-10-16 12:33:10 +0000

    Allow connecting to django host in docker compose

--- config/settings/local.py
+++ config/settings/local.py
@@ -16,7 +16,7 @@ DEBUG = True
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "django"]
 
 # CACHES
 # ------------------------------------------------------------------------------

1f9b2a78fde1083981566c953dbc20b7e0b7700a
Ryan Welch <welchr@umich.edu>
2025-12-05 21:08:04 +0000

    Refresh materialized view on data reload

--- bin/compose-reload-data.sh
+++ bin/compose-reload-data.sh
@@ -5,3 +5,4 @@ docker compose exec db bash -c 'psql -U colocus -c "DROP DATABASE core WITH (FOR
 docker compose exec db bash /docker-entrypoint-initdb.d/init-db.sh
 docker compose exec django bash -c 'source .venv/bin/activate && python3 manage.py migrate --database=core'
 docker compose exec django bash /opt/colocus/bin/entrypoint-migrate-and-load.sh
+docker compose exec db bash -c 'psql -U colocus -d core -c "REFRESH MATERIALIZED VIEW core_colocresult_with_orphans"'
